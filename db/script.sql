896bd70ab15602fb15d038c4e12e8297ca8872a4
Enrico070 <125027577+Enrico070@users.noreply.github.com>
2024-05-06 08:59:54 -0300

    feat: criação da base projeto e da script

--- db/script.sql
+++ db/script.sql
@@ -1,16 +1,29 @@
 --Criação do banco
 
-CREATE DATABASE aulaback;
+CREATE DATABASE heroisbb;
 
---Crição da tabela
+--Crição das tabelas
 
-CREATE TABLE usuarios(
+CREATE TABLE herois(
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL
+    poder VARCHAR(100) NOT NULL,
+    nivel INT NOT NULL,
+    hp INT NOT NULL,
+    ataque INT NOT NULL
+)
+
+CREATE TABLE batalhas(
+    id SERIAL PRIMARY KEY,
+    heroi1_id INT NOT NULL,
+    heroi2_id INT NOT NULL,
+    vencedor_id INT NOT NULL,
+    FOREIGN KEY (heroi1_id) REFERENCES herois(id)
+    FOREIGN KEY (heroi2_id) REFERENCES herois(id)
+    FOREIGN KEY (vencedor_id) REFERENCES herois(id)
 )
 
 --Inserção de dados teste
 
-INSERT INTO usuarios(nome, email) VALUES ('João', 'joaosantos@gmail.com');
-INSERT INTO usuarios(nome, email) VALUES ('M.Rita', 'mariarita@gmail.com');
+INSERT INTO herois(nome, email) VALUES ('João', 'joaosantos@gmail.com');
+INSERT INTO herois(nome, email) VALUES ('M.Rita', 'mariarita@gmail.com');
